dbc1e323b98efb65501b6b185c001ef02e8c19c9
MdMuzaffar <54287360+MdMuzaffar@users.noreply.github.com>
2025-06-16 23:57:19 +0530

    Using For Loop in Python

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
 print("Hello World")
+x = 1
+y = 2
+Unit_Price = 3
+
+print("Hello World")
+
+course = "Python Programming"
+print(len(course))
+
+# For Loop
+
+for number in range(4):
+    print("number", number + 1, (number + 1) * ".")
