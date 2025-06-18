4d09f240ab6294da09f4dcfb3f560d42c497b37f
MdMuzaffar <54287360+MdMuzaffar@users.noreply.github.com>
2025-06-18 23:33:45 +0530

    Python : using for loop

--- app.py
+++ app.py
@@ -10,5 +10,13 @@ print(len(course))
 
 # For Loop
 
-for number in range(4):
+for number in range(1, 4):
     print("number", number + 1, (number + 1) * ".")
+
+# count even number under 10
+count = 0
+for number in range(1, 10):
+    if number % 2 == 0:
+        count += 1
+        print(number)
+print(f"we have {count} even numbers")
