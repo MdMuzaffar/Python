ebb440cc2bb27f50d5371d96f09f693e1dd759f1
MdMuzaffar <54287360+MdMuzaffar@users.noreply.github.com>
2025-06-19 00:45:59 +0530

    Python: Passing parameters into functions

--- app.py
+++ app.py
@@ -1,22 +1,52 @@
-print("Hello World")
-x = 1
-y = 2
-Unit_Price = 3
 
-print("Hello World")
+# default argument
 
-course = "Python Programming"
-print(len(course))
+def increament(number, by=3):
+    return number + by
 
-# For Loop
 
-for number in range(1, 4):
-    print("number", number + 1, (number + 1) * ".")
+print(increament(5))
 
-# count even number under 10
-count = 0
-for number in range(1, 10):
-    if number % 2 == 0:
-        count += 1
-        print(number)
-print(f"we have {count} even numbers")
+# how to pass argument in function
+
+
+def greeting(first_name, second_name):
+    print(f"Hi {first_name} {second_name}")
+    print("Welcome on aboard")
+
+
+greeting("Muzaffar", "Ahmed")
+greeting("Afsar", "Ahmed")
+
+# create basic function
+
+
+def greet():
+    print("Hello world")
+    print("Learning Python")
+
+
+greet()
+
+# print("Hello World")
+# x = 1
+# y = 2
+# Unit_Price = 3
+
+# print("Hello World")
+
+# course = "Python Programming"
+# print(len(course))
+
+# # For Loop
+
+# for number in range(1, 4):
+#     print("number", number + 1, (number + 1) * ".")
+
+# # count even number under 10
+# count = 0
+# for number in range(1, 10):
+#     if number % 2 == 0:
+#         count += 1
+#         print(number)
+# print(f"we have {count} even numbers")
