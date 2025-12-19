0f8021fe488396c615b875dfa1c32b401607f6af
MdMuzaffar <54287360+MdMuzaffar@users.noreply.github.com>
2025-12-19 23:20:23 +0530

    Python_practice

--- app.py
+++ app.py
@@ -834,6 +834,60 @@ cloud.add("Python")
 cloud.add("Python")
 print(cloud.tags)
 
+# ---------------------------Private methods------------------------------------
+# in private methods we use __tags to keep it private
+
+# -------------------------------------Properties--------------------------------
+
+
+class Product:
+    def __init__(self, price):
+        self.price = price
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        if value < 0:
+            raise ValueError("Price cannot be negative")
+        self.__price = value
+
+    # price = property(get_price, set_price)
+
+
+product = Product(120)
+print(product.price)
+
+
+# -------------------------------------Inheritance-----------------------------
+
+class Animal:
+    def __init__(self):
+        self.age = 1
+
+    def eat(self):
+        print("eat")
+
+
+class Mammal(Animal):
+    def walk(self):
+        print("walk")
+
+
+class Fish(Animal):
+    def swim(self):
+        print("swim")
+
+
+m = Mammal()
+m.eat()
+# print(m.age)
+print(m.age)
+
+# ----------------------------------------Method Overriding-----------------------------
+
 
 # -------------------------------------Practice---------------------------------
 # -------------Basic Calculator(Variable , Input/Output, Basic arthmetic)-----------------------/
